60fef1b73a4dbe60a9bf8152f46220bb2acd7c03
leeg3 <leeg3@wit.edu>
2018-04-03 16:49:40 -0400

    Added mac stuff

--- Scanner.py
+++ Scanner.py
@@ -3,38 +3,165 @@ Author: Greg Lee
 Date: 3/24/18
 Class: Scripting for Cyber Security
 Purpose: System vulnerability scanner for Mac/Windows
-"""
 
-"""
 notes:
 steps for script:
 get list o updates
 check to see if certain critical updates are missing
 then output names of missing updates
-prompt for script to automatically install it?
+prompt for script to automatically install it? **maybe if i have time**
 done
 
 *MAC*
-
+seapea, achilles, aeris, dark matter, nightskies, triton, dark mullet
 get list o updates
-- NAH use this: /usr/sbin/system_profiler SPInstallHistoryDataType
+- **use this: /usr/sbin/system_profiler SPInstallHistoryDataType
 - this works too kinda softwareupdate -l
 
 check to see if certain critical updates are missing
-
+- need to get a list of updates that were made specifically for these exploits
+- compare list from above to list of installed updates
+https://support.apple.com/en-us/HT201222
 
 then output names of missing updates
+
 
 prompt for script to automatically install it?
 - sudo softwareupdate -i <name of update>
 done
 
 
+*Windows*
+AngelFire (Xp/7), Dumbo (x32 XP/Vista/new versions), BothanSpy, Elsa, Brutal Kangaroo, Pandemic, Athena (XP and up), AfterMidnight, Grasshopper
+
+
+get list of updates
+$Session = New-Object -ComObject "Microsoft.Update.Session"
+$Searcher = $Session.CreateUpdateSearcher()
+$historyCount = $Searcher.GetTotalHistoryCount()
+
+$Searcher.QueryHistory(0, $historyCount) | Select-Object Date,
+    @{name="Operation"; expression={switch($_.operation){
+    1 {"Installation"};}}},
+            @{name="Status"; expression={switch($_.resultcode){
+            2 {"Succeeded"}; 3 {"Succeeded With Errors"};}}}, Title, Description | Out-File -FilePath <systemfilepath>\systemupdates.txt -Encoding ascii
+#Write-Host $historyCount
+#Write-Host ([System.Environment]::CurrentDirectory)
+
+check for missing updates
+- get list o updates for Vault 7
+- apple does not say which update patched these vulnerabilities but it was patched in all Mac's made after 2013.
+
+output names of missing updates
+
+prompt script to automatically install?
+- looks like i'd need to do this to auto install updates
+https://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc
+
 """
+
+import os
+import platform
+
+#seapea[10.6, 10.7], achilles [10.6, all?], nightskies[10.5], triton [10.7, 10.8], dark mullet, Der Stare[10.8, 10.9]
+#aeris[malware], Sonic Screwdriver[firmware thing]
+#just update to 10.10 with latest Security updates?
+
+def MacScanner():
+    #get updates from system
+    minVersion = "10.20"
+    """ CHANGE BACK TO 10.10 """
+    minSecurityUpdateVersion = [2017, 3] #OG 2015, 6
+
+    print("Mac Scanner")
+
+    os.system("defaults read loginwindow SystemVersionStampAsString > SystemVersion.txt")
+
+    #check system OS. if older than 10.10 then proceed with updates check
+    print("Getting System OS version")
+    OS_version = [OS_version.rstrip('\n') for OS_version in open('SystemVersion.txt')]
+
+    temp = OS_version[0].split('.')
+    temp2 = minVersion.split('.')
+
+    if (int(temp[1]) > int(temp2[1])):
+        print("OS is good")
+        return
+    else:
+        print("OS is subject to vulnerabilities")
+
+    print("Retrieving list of installed system updates")
+    os.system("/usr/sbin/system_profiler SPInstallHistoryDataType > InstalledUpdates.txt")
+    #open file and get the update name and the version #
+    #can prob use algoirthm from CA to get the info
+    #check against array of most up to date version numbers of important updates
+    #output update names and version numbers that are less than the ones in the array
+    lines = [line.rstrip('\n') for line in open('InstalledUpdates.txt')]
+    lines2 = []
+
+    for i in range(0, len(lines)):
+        lines[i] = lines[i].strip()
+
+        if (lines[i] == '\n' or lines[i] == ""):
+            i+=1
+
+    update_names = []
+    update_version_num = []
+
+    for i in range(0, len(lines)):
+        #print(lines[i])
+        if "Security Update" in lines[i] and "Security Update" not in update_names:
+            update_names.append(lines[i].rstrip(':'))
+
+    print("The following Security Updates have been installed onto this machine: ")
+    for i in range(0,len(update_names)):
+        #print(update_names[i])
+        update_version_num.append(update_names[i].split(' '))
+
+    #print(num)
+    #print(update_names)
+    #get individual number, see how it compares to minSecurityUpdateVersion
+    #print(update_version_num[0][2])
+    uptodate_update = 0
+    uptodate_version = 0
+
+    for value in update_version_num:
+        temp = value[2].split('-')
+        #print("{} {}".format(temp[0], temp[1]))
+
+        if int(temp[0]) > minSecurityUpdateVersion[0]:
+            #print("its a new security update")
+            uptodate_update = 1
+        elif int(temp[0]) == int(minSecurityUpdateVersion[0]):
+            if int(temp[1]) > int(minSecurityUpdateVersion[1]):
+                #print("its a new security version")
+                uptodate_version = 1
+
+    if uptodate_update == 1 and uptodate_version == 1:
+        print("Your system is secure from most Vault 7 mac OS X vulnerabilties")
+    elif uptodate_version == 0:
+        print("Please apply the newest version of the security update to your machine")
+    else:
+        print("Please update OS to newest version")
+
+
+def WindowsScanner():
+    print("Windows Scanner")
+
+
+#def LinuxScanner():
+#    print("Linux Scanner")
 
 
 def main():
-    name = ""
+    system_os = platform.system()
+
+    if system_os == 'Darwin': #Mac
+        MacScanner()
+    elif system_os == 'Windows': #Windows
+        WindowsScanner()
+#    else: #Linux
+#        LinuxScanner()
 
 if __name__ == "__main__":
     main()
